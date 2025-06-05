7f59ab9146e5d2ed81233a5f5570f4479fb88111
Amanda Simonds <amandasimonds9@gmail.com>
2025-06-04 21:49:12 -0400

    basic styles setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Noto_Sans } from "next/font/google";
 import "./globals.scss";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const notoSans = Noto_Sans({
+  variable: "--font-noto-sans",
   subsets: ["latin"],
 });
 
@@ -24,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${notoSans.variable} ${notoSans.variable}`}>
         {children}
       </body>
     </html>
---
+++ src/app/navigation/navigation.tsx
@@ -0,0 +1,17 @@
+import "./navigation.scss";
+
+export default function Navigation() {
+  return (
+    <div className="navigation">
+      <div className="logo">Logo</div>
+      <div className="cta-wrapper">
+      <a href="" className="cta">Call Now 865-222-2222</a>
+      </div>
+      <div className="links">
+        <a href="">ABOUT</a>
+        <a href="">SERVICES & PRICING</a>
+        <a href="">CONTACT</a>
+      </div>
+    </div>
+  );
+}
